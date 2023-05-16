c4c2fba95cb65f412c96c4b0a3a20cc7f0a8501a
Somov62 <2003misha980@gmail.com>
2023-05-16 12:21:03 +0300

    Доделан контроллер пользователей  DAL слоя

--- Data/API/RyazanSpace.DAL.API/Controllers/UsersController.cs
+++ Data/API/RyazanSpace.DAL.API/Controllers/UsersController.cs
@@ -23,6 +23,14 @@ namespace RyazanSpace.DAL.API.Controllers
         public async Task<IActionResult> ExistId(int id) =>
             await _repository.ExistId(id) ? Ok(true) : NotFound(false);
 
+
+        [HttpGet("exist")]
+        [HttpPost("exist")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
+        public async Task<IActionResult> Exist(User item) => await _repository.Exist(item) ? Ok(true) : NotFound(false);
+
+
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAll() =>
@@ -33,6 +41,11 @@ namespace RyazanSpace.DAL.API.Controllers
         public async Task<ActionResult<IEnumerable<User>>> Get(int skip, int count) =>
             Ok(await _repository.Get(skip, count));
 
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id) => await _repository.GetById(id) is { } item ? Ok(item) : NotFound();
+
         [HttpGet("page/{pageIndex:int}/{pageSize:int}")]
         [HttpGet("page[[{pageIndex:int}:{pageSize:int}]]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -41,6 +54,35 @@ namespace RyazanSpace.DAL.API.Controllers
         {
             var result = await _repository.GetPage(pageIndex, pageSize);
             return result.Items.Any() ? Ok(result) : NotFound(result);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Add(User item)
+        {
+            var result = await _repository.Add(item);
+            return CreatedAtAction(nameof(GetById), new {id = result.Id});
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(User item)
+        {
+            if (await _repository.Update(item) is not { } result)
+                return NotFound(item);
+            return AcceptedAtAction(nameof(GetById), new { id = result.Id });
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(User item)
+        {
+            if (await _repository.Delete(item) is not { } result)
+                return NotFound(item);
+            return Ok(result);
         }
     }
 }
